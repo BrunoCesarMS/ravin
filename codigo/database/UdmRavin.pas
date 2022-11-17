c7fb74e087668a885d2cdb8fb46176eb99426be6
FURB\bcmsilva <brunocesarms@outlook.com>
2022-11-16 21:49:17 -0300

    Alterações efetuadas no dia 16/11/2022

--- codigo/database/UdmRavin.pas
+++ codigo/database/UdmRavin.pas
@@ -8,7 +8,7 @@ uses
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
   FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
   FireDAC.Comp.UI, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
-  FireDAC.DApt, FireDAC.Comp.DataSet;
+  FireDAC.DApt, FireDAC.Comp.DataSet, vcl.Dialogs;
 
 type
   TdmRavin = class(TDataModule)
@@ -16,8 +16,12 @@ type
     drvBancoDeDados: TFDPhysMySQLDriverLink;
     wtcBancoDeDados: TFDGUIxWaitCursor;
     procedure DataModuleCreate(Sender: TObject);
+    procedure cnxBancoDeDadosBeforeConnect(Sender: TObject);
+    procedure cnxBancoDeDadosAfterConnect(Sender: TObject);
   private
     { Private declarations }
+    procedure CriarTabelas();
+    procedure InserirDados();
   public
     { Public declarations }
   end;
@@ -27,13 +31,83 @@ var
 
 implementation
 
+uses UresourcesUtils;
+
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
 
+procedure TdmRavin.cnxBancoDeDadosAfterConnect(Sender: TObject);
+var
+  LCriarBaseDados: boolean;
+  LCaminhoBaseDados : string;
+begin
+  LCaminhoBaseDados := 'C:\ProgramData\MySQL\MySQL Server 8.0\Data\ravin\pessoa.ibd';
+  LCriarBaseDados := not FileExists(LCaminhoBaseDados, true);
+  if LCriarBaseDados then
+  begin
+    CriarTabelas();
+    InserirDados();
+  end;
+end;
+
+procedure TdmRavin.cnxBancoDeDadosBeforeConnect(Sender: TObject);
+var
+  LCriarBaseDados : Boolean;
+  LCaminhoBaseDados : string;
+
+begin
+  LCaminhoBaseDados := 'C:\ProgramData\MySQL\MySQL Server 8.0\Data\ravin\pessoa.ibd';
+  LCriarBaseDados := not FileExists(LCaminhoBaseDados, true);
+  with cnxBancoDeDados do
+  begin
+    Params.Values['Server'] := 'localhost';
+    Params.Values['User_Name'] := 'root';
+    Params.Values['Password'] := 'root';
+    Params.Values['DriverID'] := 'MySQL';
+    Params.Values['Port'] := '3306';
+
+    if not LCriarBaseDados then
+    begin
+      Params.Values['Database'] := 'ravin';
+    end;
+  end;
+end;
+
 procedure TdmRavin.DataModuleCreate(Sender: TObject);
 begin
-  cnxBancoDeDados.Connected := true;
+  if not cnxBancoDeDados.Connected then
+  begin
+    cnxBancoDeDados.Connected := true;
+  end;
+
+end;
+
+procedure TdmRavin.CriarTabelas;
+begin
+  try
+    cnxBancoDeDados.ExecSQL(TResourceUtils.carregararquivoresource(
+    'createTable.sql','ravinscripts'));
+  except
+    on E: Exception do
+      ShowMessage(E.Message);
+  end;
+end;
+
+procedure TdmRavin.InserirDados;
+begin
+  try
+    cnxBancoDeDados.StartTransaction();
+    cnxBancoDeDados.ExecSQL(TResourceUtils.carregararquivoresource(
+    'inserts.sql','ravinscripts'));
+    cnxBancoDeDados.Commit();
+  except
+    on E: Exception do
+    begin
+      cnxBancoDeDados.Rollback;
+      ShowMessage(E.Message)
+    end;
+  end;
 end;
 
 end.
